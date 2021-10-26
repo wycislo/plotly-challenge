2bfa8dd8833aaed8716f77fbc7c60af0c9996553
wycislo <wycislo@yahoo.com>
2021-10-25 21:48:57 -0500

    move dirs

---
+++ app.py
@@ -0,0 +1,17 @@
+# flask app to render html on local machines.
+# 
+
+import os
+from flask import Flask, render_template, redirect
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
