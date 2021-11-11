a3d82b988efd1474b248f1ec9fee6ae56de57c4f
wycislo <wycislo@yahoo.com>
2021-11-11 07:35:11 -0600

    StarterCode/static/js/app:

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,33 @@
 
+
+function getPlots(id) {
+//Read samples.json
+d3.json("samples.json").then (sampledata =>{
+    console.log(sampledata)
+    var ids = sampledata.samples[0].otu_ids;
+    console.log(ids)
+    var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+    console.log(sampleValues)
+    var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+    console.log (labels)
+// get only top 10 otu ids for the plot OTU and reversing it. 
+    var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+// get the otu id's to the desired form for the plot
+    var OTU_id = OTU_top.map(d => "OTU " + d);
+    console.log(`OTU IDS: ${OTU_id}`)
+ // get the top 10 labels for the plot
+    var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+    console.log(`OTU_labels: ${labels}`)
+    var trace = {
+        x: sampleValues,
+        y: OTU_id,
+        text: labels,
+        marker: {
+        color: 'blue'},
+        type:"bar",
+        orientation: "h",
+    };
+
  // create a function to read data for dropdwown
  function loadDropdown() {
     // select dropdown menu 
@@ -14,10 +43,10 @@
         });
 
         // call the functions to display the data and the plots to the page
-        // getPlots(data.names[0]);
+        getPlots(data.names[0]);
         loadDemoInfo(data.names[0]);
     });
-}
+
 
 
 
@@ -49,8 +78,6 @@ function loadDemoInfo(id) {
 
 
 }
-
-
 
 
 loadDropdown();
