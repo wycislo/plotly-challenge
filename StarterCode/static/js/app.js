b9eda094ab9b0efcd8c337c3c522780642dc7285
wycislo <wycislo@yahoo.com>
2021-11-11 07:00:53 -0600

    update

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,7 +6,7 @@
 
     // read the data 
     d3.json("samples.json").then((data)=> {
-        console.log(data)
+        // console.log(data)
 
         // get the id data to the dropdwown menu
         data.names.forEach(function(name) {
@@ -14,10 +14,43 @@
         });
 
         // call the functions to display the data and the plots to the page
-        getPlots(data.names[0]);
-        getDemoInfo(data.names[0]);
+        // getPlots(data.names[0]);
+        loadDemoInfo(data.names[0]);
     });
 }
+
+
+
+// get demographic info and put in sample metadata section 
+// read id from dropdown 
+function loadDemoInfo(id) {
+
+    // read data 
+    d3.json("samples.json").then((data)=> {
+        let metadata = data.metadata;
+         // console.log(metadata)
+          // filter meta data info by id
+          let result = metadata.filter(meta => meta.id.toString() === id)[0];
+          console.log(result);
+              // select sample-metadata
+        let demographicInfo = d3.select("#sample-metadata");
+        // empty the demographic info panel each time before getting new id info
+        demographicInfo.html("");
+    
+        // grab the necessary demographic data data for the id and append the info to the panel
+           Object.entries(result).forEach((key) => {   
+               demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");
+           });    
+
+    })
+
+
+
+
+}
+
+
+
 
 loadDropdown();
 
